c505092243cd72289a5103a3f944f101cd4c5141
Jack Valmadre <jack.valmadre@gmail.com>
2014-07-21 15:37:08 -0400

    cg: Add practical test (dims = 1000)

--- cg/cg_test.go
+++ cg/cg_test.go
@@ -1,6 +1,18 @@
 package cg
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"testing"
+
+	"github.com/gonum/blas/goblas"
+	"github.com/gonum/matrix/mat64"
+)
+
+func init() {
+	mat64.Register(goblas.Blas{})
+}
 
 func ExampleSolve() {
 	a := func(x []float64) []float64 {
@@ -37,4 +49,60 @@ func ExampleSeq() {
 	fmt.Println(seq.Solution())
 	// Output:
 	// [2 3]
+}
+
+func TestSolve(t *testing.T) {
+	const n = 1000
+	want := randVec(n)
+	a := randMat(n)
+	// f(x) = A x
+	f := func(x []float64) []float64 {
+		xvec := mat64.Vec(x)
+		y := mat64.Vec(make([]float64, n))
+		y.Mul(a, &xvec)
+		return y
+	}
+	b := f(want)
+	x0 := make([]float64, n)
+
+	got, err := Solve(f, b, x0, 0, n/10)
+	if err != nil {
+		t.Fatal("error:", err)
+	}
+	checkEqual(t, want, got, 1e-6)
+}
+
+func randVec(n int) []float64 {
+	x := make([]float64, n)
+	for i := range x {
+		x[i] = rand.NormFloat64()
+	}
+	return x
+}
+
+func randMat(n int) mat64.Matrix {
+	m := 2 * n
+	v := mat64.NewDense(m, n, nil)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			v.Set(i, j, rand.NormFloat64())
+		}
+	}
+	// A <- V' V
+	a := mat64.NewDense(n, n, nil)
+	vt := mat64.NewDense(n, m, nil)
+	vt.TCopy(v)
+	a.Mul(vt, v)
+	return a
+}
+
+func checkEqual(t *testing.T, want, got []float64, eps float64) {
+	if len(want) != len(got) {
+		t.Fatalf("lengths differ: want %d, got %d", len(want), len(got))
+	}
+	for i := range want {
+		if math.Abs(want[i]-got[i]) > eps {
+			t.Errorf("different: at %d: want %g, got %g", i, want[i], got[i])
+		}
+	}
 }
